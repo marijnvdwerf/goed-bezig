bd118eaeef094a94b1cdf89e2957f31910327f9a
DJUTCH <hakim1991@hotmail.com>
2013-05-14 10:38:59 +0200

    save checkin venue json

--- simulate/foursquare/checkin.php
+++ simulate/foursquare/checkin.php
@@ -1,4 +1,6 @@
 <?php
 
+$checkin = json_decode($_POST['checkin']);
+file_put_contents('checkins/' . $checkin->venue->name . '.json', $_POST['checkin']);
 
 file_put_contents('data.json', json_encode($_POST, JSON_PRETTY_PRINT));
