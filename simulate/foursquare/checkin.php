55a51d20a926f8ad1d4cb48d95128fbf1ad972ea
DJUTCH <hakim1991@hotmail.com>
2013-05-14 10:58:45 +0200

    save user checkin json

--- simulate/foursquare/checkin.php
+++ simulate/foursquare/checkin.php
@@ -3,4 +3,7 @@
 $checkin = json_decode($_POST['checkin']);
 file_put_contents('checkins/' . $checkin->venue->name . '.json', $_POST['checkin']);
 
+$user = json_decode($_POST['user']);
+file_put_contents('users/' . $user->firstName . '.json', $_POST['user']);
+
 file_put_contents('data.json', json_encode($_POST, JSON_PRETTY_PRINT));
