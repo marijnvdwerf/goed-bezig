781d7e80f3097710c45c8b6657e99d185b8614dc
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2013-05-23 00:48:38 +0200

    Flip card on tap

--- prototypes/cards/scripts/cards.js
+++ prototypes/cards/scripts/cards.js
@@ -1,4 +1,22 @@
 var cardContainer = $('#card-container');
+var overlay = {
+    el: $('.overlay'),
+
+    show: function () {
+        this.el.css('display', 'block');
+
+        window.setTimeout(function (el) {
+            el.css('opacity', 1);
+        }, 1, this.el);
+    },
+
+    hide: function () {
+        this.el.css('opacity', 0);
+        this.el[0].addEventListener('webkitTransitionEnd', function (e) {
+            $(this).css('display', 'none');
+        });
+    }
+};
 
 $('.card').each(function (index, card) {
     card = $(card);
@@ -9,4 +27,10 @@ $('body').removeClass('loading');
 
 cardContainer.masonry({
     itemSelector: '.card-wrapper'
+});
+
+cardContainer.hammer().on('tap', '.card', function (event) {
+    overlay.show();
+    var card = $(this);
+    card.parent().addClass('focus');
 });
