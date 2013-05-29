3fbe45c169dd462317b3411f3a6ff9ca40742b4b
Pim Vet <pimvet@live.nl>
2013-05-29 12:00:54 +0200

    Fixed "Added create-example database"

--- app/index.php
+++ app/index.php
@@ -242,7 +242,7 @@ $app->get('/db/create-example', function() use($app){
     $address ->user = $user;
     $address ->address = "Hoofdstraat 5";
 
-    R::store($address)
+    R::store($address);
 
 
 });
