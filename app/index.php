b6db0113038f02d6a26ee5b6289e90c153c5e490
Pim Vet <pimvet@live.nl>
2013-06-04 10:11:09 +0200

    Created additional file for the create database function

--- app/index.php
+++ app/index.php
@@ -3,6 +3,7 @@
 require 'vendor/autoload.php';
 require 'includes/config.php';
 require 'includes/vendor/rb.php';
+require 'includes/createdatabase.php';
 
 
 R::setup('mysql:host=mysql.dd;dbname=' . $db_name, $db_user, $db_pass);
@@ -227,119 +228,9 @@ $app->get('/db/reset', function() use($app) {
         
 });
 
-$app->get('/db/create-example', function() use($app){
-    
-    $user = R::dispense('user');
-    $user ->name = 'John Doe';
-    $user ->email = "goedbezig@marijnvdwerf.nl";
-    $user ->sex = "Male";
-    $user ->age = 45;
-    $user ->foursquareId = "00023043287276367263";
-    $user ->foursquareToken = "RJJEHFDSHJFHJKHF34938598KJHFKJSHFJKHFJHSF9843UIHFJHSFJSIFH04823DHJ";
-    $user ->facebookId = "01005002392872387482";
-    $user ->facebookToken = "AJFNH764JHFJHFJ583HFHRJH987398479FHISJKJFGJ3476HDGJHGFJ98724JHGFJG";
-    $user ->setMeta('cast.foursquareId', 'string');
-    $user ->setMeta('cast.facebookId', 'string');
-    R::store($user);
-
-        $address = R::dispense('address');
-        //$user ->ownAddress = [$adress];
-        $address ->user = $user;
-        $address ->address = "Hoofdstraat 5";
-        $address ->postalCode = "1234AB";
-        $address ->town = "Amsterdam";
-        R::store($address);
-
-
-    $achievement = R::dispense('achievement');
-    $achievement ->name = "Sporter";
-    $achievement ->description = "Je bent sportief bezig";
-    $achievement ->icon = "sporter";
-    $achievement ->mystery = false;
-    R::store($achievement);
-
-        $requirement = R::dispense('requirement');
-        $requirement ->achievement = $achievement;
-        $requirement ->venueType = "College Gym; Gym";
-        $requirement ->numberRequired = 2;
-        R::store($requirement);
-
-        $userAchievement = R::dispense('userachievement');
-        $userAchievement ->user = $user;
-        $userAchievement ->achievement = $achievement;
-        $userAchievement ->progress = 0.80;
-        $userAchievement ->goodieClaimed = null;
-        R::store($userAchievement);
-
-            $stamp = R::dispense('stamp');
-            $stamp ->userachievement = $userAchievement;
-            $stamp ->venueType = "Pool / Lake";
-            $stamp ->requirement = $requirement;
-            $stamp ->datetime = new DateTime();
-            R::store($stamp);
-
-        $requirement = R::dispense('requirement');
-        $requirement ->achievement = $achievement;
-        $requirement ->venueType = "Pool / Lake;Waterpark;Gym Pool";
-        $requirement ->numberRequired = 10;
-        R::store($requirement);
-
-            $stamp = R::dispense('stamp');
-            $stamp ->userachievement = $userAchievement;
-            $stamp ->venueType = "Gym";
-            $stamp ->requirement = $requirement;
-            $stamp ->datetime = new DateTime();
-            R::store($stamp);
-
-    $achievement = R::dispense('achievement');
-    $achievement ->name = "Waterrat";
-    $achievement ->description = "Je bent graag in het water";
-    $achievement ->icon = "waterrat";
-    $achievement ->mystery = false;
-    R::store($achievement);
-
-        $goodie = R::dispense('goodie');
-        $goodie ->achievement = $achievement;
-        $goodie ->name = "Bidon";
-        $goodie ->description = "Een lekkere drinkfles";
-        $goodie ->icon = "bidon";
-        $goodie ->mystery = false;
-        R::store($goodie);
-
-        $requirement = R::dispense('requirement');
-        $requirement ->achievement = $achievement;
-        $requirement ->venueType = "College Gym; Gym";
-        $requirement ->numberRequired = 10;
-        R::store($requirement);
-
-        $userAchievement = R::dispense('userachievement');
-        $userAchievement ->user = $user;
-        $userAchievement ->achievement = $achievement;
-        $userAchievement ->progress = 0.80;
-        $userAchievement ->goodieClaimed = null;
-        R::store($userAchievement);
-
-            $stamp = R::dispense('stamp');
-            $stamp ->userachievement = $userAchievement;
-            $stamp ->venueType = "Pool / Lake";
-            $stamp ->requirement = $requirement;
-            $stamp ->datetime = new DateTime();
-            R::store($stamp);
-
-        $requirement = R::dispense('requirement');
-        $requirement ->achievement = $achievement;
-        $requirement ->venueType = "Pool / Lake;Waterpark;Gym Pool";
-        $requirement ->numberRequired = 3;
-        R::store($requirement);
-
-            $stamp = R::dispense('stamp');
-            $stamp ->userachievement = $userAchievement;
-            $stamp ->venueType = "Gym";
-            $stamp ->requirement = $requirement;
-            $stamp ->datetime = new DateTime();
-            R::store($stamp);
-
-
+$app->get('/db/create-example', function() use ($app){
+    //function in 'includes/createdatabase.php'
+    create_db();
 });
 
 $app->post('/checkin/foursquare', function() use($app){
