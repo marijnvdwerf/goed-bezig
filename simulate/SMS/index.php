b47747e61053b0e0fed283ad7abb0106efb4122c
Dylan <dylan@Macintosh.local>
2013-06-10 16:18:38 +0200

    Mollie SMS API
    script for sending precomposed SMS using the Mollie API

---
+++ simulate/SMS/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="sendsms.php" method="post">
+    telefoonnummer 06-: <input type="text" name="telnummer">
+    <input type="submit">
+</form>
+
+</body>
+</html>
---
+++ simulate/SMS/sendsms.php
@@ -0,0 +1,41 @@
+<?php
+/*
+  =======================================================================
+   File:     sendsms.php
+   Created:  16-01-2005
+   Author:   Mollie B.V.
+   Version:  v 2.2 09-03-2009
+
+   More info? Go to http://www.mollie.nl/
+  ========================================================================
+*/
+require('classes/class.mollie.php');
+// or php4: require('classes/class.mollie-php4.php');
+  $sms = new mollie();
+
+  // Choose SMS gateway
+  $sms->setGateway(2);
+  // Set Mollie.nl username and password
+  $sms->setLogin('dylandejonge', 'gekkebrabo1');
+  // Set afzender van het bericht
+  $sms->setOriginator('Goedbezig');
+  // Add ontvanger(s) van het bericht
+  $sms->addRecipients('316'.($_POST["telnummer"]));
+  // Add reference (needed for delivery reports)
+  $sms->setReference('1234');
+  $name = 'Dylan'
+  $achievementName = 'Waterrat';
+  // Send the SMS Message
+  $sms->sendSMS('Gefeliciteerd'.$name.', je hebt de achievement '.$achievementName.' behaald!');
+
+  if ($sms->getSuccess()) {
+    echo '<b>SMS message is sent to '.$sms->getSuccessCount().' number(s)!</b>';
+  }
+  else {
+    echo '<b>Sending the message has failed!</b><br>
+        Errorcode: ' . $sms->getResultCode() . '<br>
+        Errormessage: ' . $sms->getResultMessage();
+  }
+print_r($sms);
+?>
+
