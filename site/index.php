8b6fd1c65ba26a4226872774bf942867ba62a88e
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2013-06-11 22:19:35 +0200

    Open Foursquare login when button is pressed
    Uses jsUri library from https://github.com/derek-watson/jsUri

---
+++ site/index.php
@@ -0,0 +1,44 @@
+<?php
+
+require 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>GoedBezig</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body id="home">
+
+
+    <div class="page page-login" data-state="logo">
+        <h1 class="logo">Inloggen</h1>
+
+        <div class="form">
+            <button class="button-facebook">Facebook</button>
+            <button class="button-foursquare">Foursquare</button>
+        </div>
+    </div>
+
+    <div class="page page-main">
+        <h1>Main</h1>
+    </div>
+
+    <div class="page page-settings">
+        <h1>Settings</h1>
+    </div>
+
+    <script>
+        window.config = {
+            foursquare_id: <?= json_encode($foursquare_client_id); ?>
+        };
+    </script>
+    <script src="components/jquery/jquery.min.js"></script>
+    <script src="components/jsUri/Uri.js"></script>
+    <script src="js/script.js"></script>
+
+</body>
+</html>
--- site/js/script.js
+++ site/js/script.js
@@ -23,3 +23,21 @@ function getExtraViewportHeight() {
 $('button').click(function(){
     //$('.page-login').css('display','none');
 });
+
+
+$('.button-foursquare').on('click', function () {
+    var appUri = new Uri(window.location.href);
+    // remove ? and # parts
+    appUri
+        .setAnchor('')
+        .setQuery('');
+
+    var foursquareLoginUri = new Uri('https://foursquare.com/oauth2/authenticate');
+    foursquareLoginUri
+        .addQueryParam('client_id', config.foursquare_id)
+        .addQueryParam('response_type', 'token')
+        .addQueryParam('redirect_uri', appUri);
+
+    // open page
+    window.location.href = foursquareLoginUri.toString();
+});
