e6e0a498347018e815515a051a00272eac2c96f8
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2013-06-12 23:53:03 +0200

    Add stamps for checkins

--- app/test/ApplicationTest.php
+++ app/test/ApplicationTest.php
@@ -58,6 +58,38 @@ class ApplicationTest extends PHPUnit_Framework_TestCase
         $this->assertSame('2', $userB->id);
     }
 
+    function testAddingFoursquareCheckin()
+    {
+        $checkinData = '{"id":"51a0ecad498ec1fa9824f0ff","createdAt":1369500845,"type":"checkin","timeZone":
+        "Europe\/Amsterdam","timeZoneOffset":120,"user":{"id":"55629080","firstName":"Jeroen","lastName":
+        "van der Sanden","gender":"male","relationship":"self","photo":"https:\/\/foursquare.com\/img\/blank_boy.png",
+        "tips":{"count":0},"lists":{"groups":[{"type":"created","count":1,"items":[]}]},"homeCity":"Eindhoven, 06",
+        "bio":"","contact":{"email":"trend@marijnvdwerf.nl","facebook":"100005427698197"}},"venue":{"id":
+        "4cb4aafc75ebb60c2a53e4ad","name":"Delphis Womens Health Club","contact":{"phone":"0235257833","formattedPhone":
+        "023 525 7833"},"location":{"address":"Zijlweg 314a+b","lat":52.386945,"lng":4.610468,"postalCode":"2015 CP",
+        "city":"Haarlem","state":"Noord-Holland","country":"The Netherlands","cc":"NL"},"canonicalUrl":
+        "https:\/\/foursquare.com\/v\/delphis-womens-health-club\/4cb4aafc75ebb60c2a53e4ad","categories":[{"id":
+        "4bf58dd8d48988d176941735","name":"Gym","pluralName":"Gyms","shortName":"Gym","icon":
+        "https:\/\/foursquare.com\/img\/categories\/building\/gym.png","parents":["Shop & Service",
+        "Gym \/ Fitness Center"],"primary":true}],"verified":false,"stats":{"checkinsCount":387,"usersCount":26,
+        "tipCount":1},"likes":{"count":0,"groups":[]},"beenHere":{"count":0}}}';
+        $checkinData = json_decode($checkinData);
+
+        $jeroenId = '55629080';
+        $this->app->addFourSquareCheckin($jeroenId, $checkinData);
+        $jeroen = $this->app->getUserForFoursquareId($jeroenId);
+        $this->assertCount(2, $jeroen->ownUserachievement);
+        $this->assertEquals(0.25, $jeroen->ownUserachievement[3]->getProgress());
+        $this->assertEquals(0.5, $jeroen->ownUserachievement[4]->getProgress());
+
+        $johnId = '00023043287276367263';
+        $this->app->addFourSquareCheckin($johnId, $checkinData);
+        $john = $this->app->getUserForFoursquareId($johnId);
+        $this->assertCount(2, $john->ownUserachievement);
+        $this->assertEquals(0.75, $john->ownUserachievement[1]->getProgress());
+        $this->assertEquals(1, $john->ownUserachievement[2]->getProgress());
+    }
+
     function testNotificationGeneration()
     {
         $message = $this->app->getNotificationMessage(1, 'achievement-earned', 1);
